d54438eff13cfc3f11ad6d91a46201b9784a1336
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-10-29 12:00:08 -0400

    add types to session schema
    Co-authored-by: apersaud-dev <andrew.j.persaud@gmail.com>

--- src/data/schema/session.ts
+++ src/data/schema/session.ts
@@ -1,6 +1,23 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
-const SessionSchema = new mongoose.Schema({});
+const SessionSchema = new mongoose.Schema({
+  total_reps: {
+    type: Schema.Types.Number,
+    required: true,
+  },
+  reps_per_set: {
+    type: Schema.Types.Number,
+    required: true,
+  },
+  total_sets: {
+    type: Schema.Types.Number,
+    required: true,
+  },
+  compleatedTotalReps: {
+    type: Schema.Types.Boolean,
+    required: true,
+  },
+});
 
 const Session = mongoose.model('Session', SessionSchema);
 
--- src/data/schema/user.ts
+++ src/data/schema/user.ts
@@ -1,26 +1,29 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
   email: {
-    type: String,
+    type: Schema.Types.String,
     required: true,
   },
   password: {
-    type: String,
+    type: Schema.Types.String,
     required: true,
   },
   name: {
-    type: String,
+    type: Schema.Types.String,
     required: true,
   },
   single_set_max: {
-    type: [Number],
+    type: [Schema.Types.Number],
     required: false,
   },
-  sessions: {
-    type: 
-    required: false
-  },
+  sessions: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Session',
+      required: false,
+    },
+  ],
 });
 
 const User = mongoose.model('User', UserSchema);
