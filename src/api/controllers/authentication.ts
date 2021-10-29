3d2d0e819af6b44a75bad3c40004450b097b4e05
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-10-29 12:18:47 -0400

    wrote error messages for failed validation
    Co-authored-by: apersaud-dev <andrew.j.persaud@gmail.com>

--- src/api/controllers/authentication.ts
+++ src/api/controllers/authentication.ts
@@ -1,24 +1,35 @@
-import { Request, Response } from 'express';
+import { json, Request, Response } from 'express';
 import { isEmail, isValidPassword } from '../util/validators';
 
 export async function registration(req: Request, res: Response): Promise<void> {
   const { email, password, name } = req.body;
 
   if (email.length <= 0 || !isEmail(email)) {
-    res.status(500);
+    res.status(400).json({
+      error: 'Please enter a valid email.',
+    });
   }
 
   if (password.length <= 0 || !isValidPassword(password)) {
-    res.status(500);
+    res.status(400).json({
+      error: 'Please enter a valid password.',
+    });
   }
 
   if (name.length <= 0) {
-    res.status(500);
+    res.status(400).json({
+      error: 'You must enter a name.',
+    });
   }
 
   try {
-    res.status(200).send('Ok');
+    res.status(200).json({
+      message: 'User successfully created.',
+      payload: {},
+    });
   } catch {
-    res.status(500);
+    res.status(500).json({
+      error: 'There was an error processing your request.',
+    });
   }
 }
